c2f182f31f0bec5825d2191db6f499993eb331e9
Mathieu TUDISCO <dev@mathieutu.ovh>
2019-02-07 22:37:54 +0100

    🐛 Add empty line at end of package.json in proxy generation. (fix #5)

--- src/utils/proxy.ts
+++ src/utils/proxy.ts
@@ -3,14 +3,18 @@ import { resolveApp } from './paths'
 
 const filePath = resolveApp('package.json')
 
+const writeToFile = (filePath: string, packageJson: object) => {
+  writeFileSync(filePath, JSON.stringify(packageJson, null, 2) + '\n')
+}
+
 export const addToPackageJson = (proxy: string) => {
   const packageJson = { ...require(filePath), proxy }
 
-  writeFileSync(filePath, JSON.stringify(packageJson, null, 2))
+  writeToFile(filePath, packageJson)
 }
 
 export const removeFromPackageJson = () => {
   const { proxy, ...packageJson } = require(filePath)
 
-  writeFileSync(filePath, JSON.stringify(packageJson, null, 2))
+  writeToFile(filePath, packageJson)
 }
